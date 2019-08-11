a5e06da6db12232fb4d118f9d0f3cbd3ac95976a
k-ye <yekuang.ky@gmail.com>
2019-08-11 10:24:46 +0900

    vscode reformat

--- Stanford/CS224n/a3/parser_model.py
+++ Stanford/CS224n/a3/parser_model.py
@@ -13,6 +13,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class ParserModel(nn.Module):
     """ Feedforward neural network with an embedding layer and single hidden layer.
     The ParserModel will predict which transition should be applied to a
@@ -30,8 +31,9 @@ class ParserModel(nn.Module):
             in other ParserModel methods.
         - For further documentation on "nn.Module" please see https://pytorch.org/docs/stable/nn.html.
     """
+
     def __init__(self, embeddings, n_features=36,
-        hidden_size=200, n_classes=3, dropout_prob=0.5):
+                 hidden_size=200, n_classes=3, dropout_prob=0.5):
         """ Initialize the parser model.
 
         @param embeddings (Tensor): word embeddings (num_words, embedding_size)
@@ -46,34 +48,35 @@ class ParserModel(nn.Module):
         self.dropout_prob = dropout_prob
         self.embed_size = embeddings.shape[1]
         self.hidden_size = hidden_size
-        self.pretrained_embeddings = nn.Embedding(embeddings.shape[0], self.embed_size)
-        self.pretrained_embeddings.weight = nn.Parameter(torch.tensor(embeddings))
+        self.pretrained_embeddings = nn.Embedding(
+            embeddings.shape[0], self.embed_size)
+        self.pretrained_embeddings.weight = nn.Parameter(
+            torch.tensor(embeddings))
 
-        ### YOUR CODE HERE (~5 Lines)
-        ### TODO:
-        ###     1) Construct `self.embed_to_hidden` linear layer, initializing the weight matrix
-        ###         with the `nn.init.xavier_uniform_` function with `gain = 1` (default)
-        ###     2) Construct `self.dropout` layer.
-        ###     3) Construct `self.hidden_to_logits` linear layer, initializing the weight matrix
-        ###         with the `nn.init.xavier_uniform_` function with `gain = 1` (default)
+        # YOUR CODE HERE (~5 Lines)
+        # TODO:
+        # 1) Construct `self.embed_to_hidden` linear layer, initializing the weight matrix
+        # with the `nn.init.xavier_uniform_` function with `gain = 1` (default)
+        # 2) Construct `self.dropout` layer.
+        # 3) Construct `self.hidden_to_logits` linear layer, initializing the weight matrix
+        # with the `nn.init.xavier_uniform_` function with `gain = 1` (default)
         ###
-        ### Note: Here, we use Xavier Uniform Initialization for our Weight initialization.
-        ###         It has been shown empirically, that this provides better initial weights
-        ###         for training networks than random uniform initialization.
-        ###         For more details checkout this great blogpost:
-        ###             http://andyljones.tumblr.com/post/110998971763/an-explanation-of-xavier-initialization 
-        ### Hints:
-        ###     - After you create a linear layer you can access the weight
-        ###       matrix via:
-        ###         linear_layer.weight
+        # Note: Here, we use Xavier Uniform Initialization for our Weight initialization.
+        # It has been shown empirically, that this provides better initial weights
+        # for training networks than random uniform initialization.
+        # For more details checkout this great blogpost:
+        # http://andyljones.tumblr.com/post/110998971763/an-explanation-of-xavier-initialization
+        # Hints:
+        # - After you create a linear layer you can access the weight
+        # matrix via:
+        # linear_layer.weight
         ###
-        ### Please see the following docs for support:
-        ###     Linear Layer: https://pytorch.org/docs/stable/nn.html#torch.nn.Linear
-        ###     Xavier Init: https://pytorch.org/docs/stable/nn.html#torch.nn.init.xavier_uniform_
-        ###     Dropout: https://pytorch.org/docs/stable/nn.html#torch.nn.Dropout
+        # Please see the following docs for support:
+        # Linear Layer: https://pytorch.org/docs/stable/nn.html#torch.nn.Linear
+        # Xavier Init: https://pytorch.org/docs/stable/nn.html#torch.nn.init.xavier_uniform_
+        # Dropout: https://pytorch.org/docs/stable/nn.html#torch.nn.Dropout
 
-
-        ### END YOUR CODE
+        # END YOUR CODE
 
     def embedding_lookup(self, t):
         """ Utilize `self.pretrained_embeddings` to map input `t` from input tokens (integers)
@@ -91,23 +94,21 @@ class ParserModel(nn.Module):
             @return x (Tensor): tensor of embeddings for words represented in t
                                 (batch_size, n_features * embed_size)
         """
-        ### YOUR CODE HERE (~1-3 Lines)
-        ### TODO:
-        ###     1) Use `self.pretrained_embeddings` to lookup the embeddings for the input tokens in `t`.
-        ###     2) After you apply the embedding lookup, you will have a tensor shape (batch_size, n_features, embedding_size).
-        ###         Use the tensor `view` method to reshape the embeddings tensor to (batch_size, n_features * embedding_size)
+        # YOUR CODE HERE (~1-3 Lines)
+        # TODO:
+        # 1) Use `self.pretrained_embeddings` to lookup the embeddings for the input tokens in `t`.
+        # 2) After you apply the embedding lookup, you will have a tensor shape (batch_size, n_features, embedding_size).
+        # Use the tensor `view` method to reshape the embeddings tensor to (batch_size, n_features * embedding_size)
         ###
-        ### Note: In order to get batch_size, you may need use the tensor .size() function:
-        ###         https://pytorch.org/docs/stable/tensors.html#torch.Tensor.size
+        # Note: In order to get batch_size, you may need use the tensor .size() function:
+        # https://pytorch.org/docs/stable/tensors.html#torch.Tensor.size
         ###
-        ###  Please see the following docs for support:
-        ###     Embedding Layer: https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
-        ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
+        # Please see the following docs for support:
+        # Embedding Layer: https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
+        # View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
 
-
-        ### END YOUR CODE
+        # END YOUR CODE
         return x
-
 
     def forward(self, t):
         """ Run the model forward.
@@ -128,20 +129,19 @@ class ParserModel(nn.Module):
         @return logits (Tensor): tensor of predictions (output after applying the layers of the network)
                                  without applying softmax (batch_size, n_classes)
         """
-        ###  YOUR CODE HERE (~3-5 lines)
-        ### TODO:
-        ###     1) Apply `self.embedding_lookup` to `t` to get the embeddings
-        ###     2) Apply `embed_to_hidden` linear layer to the embeddings
-        ###     3) Apply relu non-linearity to the output of step 2 to get the hidden units.
-        ###     4) Apply dropout layer to the output of step 3.
-        ###     5) Apply `hidden_to_logits` layer to the output of step 4 to get the logits.
+        # YOUR CODE HERE (~3-5 lines)
+        # TODO:
+        # 1) Apply `self.embedding_lookup` to `t` to get the embeddings
+        # 2) Apply `embed_to_hidden` linear layer to the embeddings
+        # 3) Apply relu non-linearity to the output of step 2 to get the hidden units.
+        # 4) Apply dropout layer to the output of step 3.
+        # 5) Apply `hidden_to_logits` layer to the output of step 4 to get the logits.
         ###
-        ### Note: We do not apply the softmax to the logits here, because
-        ### the loss function (torch.nn.CrossEntropyLoss) applies it more efficiently.
+        # Note: We do not apply the softmax to the logits here, because
+        # the loss function (torch.nn.CrossEntropyLoss) applies it more efficiently.
         ###
-        ### Please see the following docs for support:
-        ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
+        # Please see the following docs for support:
+        # ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
 
-
-        ### END YOUR CODE
+        # END YOUR CODE
         return logits
