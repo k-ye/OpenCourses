f2080e1abb80d7911022cc3ff48c49374c0005c5
k-ye <yekuang.ky@gmail.com>
2019-08-12 14:06:53 +0900

    format code

--- Stanford/CS224n/a4/model_embeddings.py
+++ Stanford/CS224n/a4/model_embeddings.py
@@ -11,10 +11,12 @@ Anand Dhoot <anandd@stanford.edu>
 
 import torch.nn as nn
 
-class ModelEmbeddings(nn.Module): 
+
+class ModelEmbeddings(nn.Module):
     """
     Class that converts input words to their embeddings.
     """
+
     def __init__(self, embed_size, vocab):
         """
         Init the Embedding layers.
@@ -33,25 +35,22 @@ class ModelEmbeddings(nn.Module):
         src_pad_token_idx = vocab.src['<pad>']
         tgt_pad_token_idx = vocab.tgt['<pad>']
 
-        ### YOUR CODE HERE (~2 Lines)
-        ### TODO - Initialize the following variables:
-        ###     self.source (Embedding Layer for source language)
-        ###     self.target (Embedding Layer for target langauge)
+        # YOUR CODE HERE (~2 Lines)
+        # TODO - Initialize the following variables:
+        # self.source (Embedding Layer for source language)
+        # self.target (Embedding Layer for target langauge)
         ###
-        ### Note:
-        ###     1. `vocab` object contains two vocabularies:
-        ###            `vocab.src` for source
-        ###            `vocab.tgt` for target
-        ###     2. You can get the length of a specific vocabulary by running:
-        ###             `len(vocab.<specific_vocabulary>)`
-        ###     3. Remember to include the padding token for the specific vocabulary
-        ###        when creating your Embedding.
+        # Note:
+        # 1. `vocab` object contains two vocabularies:
+        # `vocab.src` for source
+        # `vocab.tgt` for target
+        # 2. You can get the length of a specific vocabulary by running:
+        # `len(vocab.<specific_vocabulary>)`
+        # 3. Remember to include the padding token for the specific vocabulary
+        # when creating your Embedding.
         ###
-        ### Use the following docs to properly initialize these variables:
-        ###     Embedding Layer:
-        ###         https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
-        
+        # Use the following docs to properly initialize these variables:
+        # Embedding Layer:
+        # https://pytorch.org/docs/stable/nn.html#torch.nn.Embedding
 
-        ### END YOUR CODE
-
-
+        # END YOUR CODE
