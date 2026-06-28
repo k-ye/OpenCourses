18b4b553ddb24afaffb3f163626a9c1d15a31bf8
Ye Kuang <yekuang.ky@gmail.com>
2026-06-28 19:35:33 +0800

    failing

--- Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
@@ -1,15 +1,62 @@
 import regex as re
+import os
+from typing import BinaryIO
 import multiprocessing as mp
 from concurrent.futures import Future, ProcessPoolExecutor, as_completed
 from collections import defaultdict
-
-from .pretokenization_example import find_chunk_boundaries
 
 type BytePair = tuple[bytes, bytes]
 type TokenCountMap = defaultdict[tuple[bytes, ...], int]
 
 
-def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]) -> TokenCountMap:
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+def pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]) -> TokenCountMap:
     special_tokens = sorted(special_tokens, key=lambda t: -len(t))
     re_special_tokens = "|".join([re.escape(t) for t in special_tokens])
     with open(input_path, "rb") as f:
@@ -56,7 +103,7 @@ def train_bpe_tokenizer(
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         futs: list[Future[TokenCountMap]] = []
         for begin, end in zip(boundaries[:-1], boundaries[1:]):
-            f = executor.submit(_pretokenize, input_path, begin, end, special_tokens)
+            f = executor.submit(pretokenize, input_path, begin, end, special_tokens)
             futs.append(f)
         for f in as_completed(futs):
             counts = f.result()
@@ -104,3 +151,4 @@ def train_bpe_tokenizer(
         total_count_after = sum(new_token_counts.values())
         assert total_count_before == total_count_after
         token_counts = new_token_counts
+    return vocabulary, merges
--- Stanford/CS336/assignment1-basics/tests/adapters.py
+++ Stanford/CS336/assignment1-basics/tests/adapters.py
@@ -9,6 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+from cs336_basics.bpe import train_bpe_tokenizer
 
 def run_linear(
     d_in: int,
@@ -589,4 +590,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return train_bpe_tokenizer(input_path, vocab_size, special_tokens)
