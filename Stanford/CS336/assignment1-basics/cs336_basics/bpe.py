0497c9c883b4e98b7e8fb11fdfbc1d2c0f4441b2
Ye Kuang <yekuang.ky@gmail.com>
2026-06-28 17:20:49 +0800

    fix

--- Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
@@ -7,14 +7,13 @@ from .pretokenization_example import find_chunk_boundaries
 
 
 def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]):
-    special_tokens.sort(key=lambda t: -len(t))
+    special_tokens = sorted(special_tokens, key=lambda t: -len(t))
     re_special_tokens = "|".join([re.escape(t) for t in special_tokens])
-    with open(input_path) as f:
+    with open(input_path, "rb") as f:
         f.seek(begin)
-        chunk = f.read(end - begin)
+        chunk = f.read(end - begin).decode("utf-8", errors="ignore")
         chunk_splits = re.split(re_special_tokens, chunk)
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    pretokens = []
     counts = defaultdict(int)
     for s in chunk_splits:
         pretokens = re.findall(PAT, s)
@@ -33,10 +32,10 @@ def train_bpe_tokenizer(
     num_workers = mp.cpu_count()
     desired_num_chunks = num_workers * 2
     with open(input_path, "rb") as f:
-        boundaries = find_chunk_boundaries(f, desired_num_chunks, special_tokens[0])
+        boundaries = find_chunk_boundaries(f, desired_num_chunks, special_tokens[0].encode("utf-8"))
 
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         futs = []
-        for i in range(len(boundaries) - 1):
-            f = executor.submit(_pretokenize, input_path, boundaries[i], boundaries[i + 1], special_tokens)
+        for begin, end in zip(boundaries[:-1], boundaries[1:]):
+            f = executor.submit(_pretokenize, input_path, begin, end, special_tokens)
             futs.append(f)
