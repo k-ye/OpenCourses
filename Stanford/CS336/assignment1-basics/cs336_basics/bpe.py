e1a269d1bb138f25dd0cf83151f84af941c5a503
Ye Kuang <yekuang.ky@gmail.com>
2026-06-28 17:28:40 +0800

    fix

--- Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
@@ -1,12 +1,14 @@
 import regex as re
 import multiprocessing as mp
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed
 from collections import defaultdict
 
 from .pretokenization_example import find_chunk_boundaries
 
+PretokenizeResult = defaultdict[str, int]
 
-def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]):
+
+def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]) -> PretokenizeResult:
     special_tokens = sorted(special_tokens, key=lambda t: -len(t))
     re_special_tokens = "|".join([re.escape(t) for t in special_tokens])
     with open(input_path, "rb") as f:
@@ -14,9 +16,9 @@ def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str
         chunk = f.read(end - begin).decode("utf-8", errors="ignore")
         chunk_splits = re.split(re_special_tokens, chunk)
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    counts = defaultdict(int)
+    counts: PretokenizeResult = defaultdict(int)
     for s in chunk_splits:
-        pretokens = re.findall(PAT, s)
+        pretokens: list[str] = re.findall(PAT, s)
         for tok in pretokens:
             counts[tok] += 1
     return counts
@@ -34,8 +36,13 @@ def train_bpe_tokenizer(
     with open(input_path, "rb") as f:
         boundaries = find_chunk_boundaries(f, desired_num_chunks, special_tokens[0].encode("utf-8"))
 
+    pretoken_counts: PretokenizeResult = defaultdict(int)
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        futs = []
+        futs: list[Future[PretokenizeResult]] = []
         for begin, end in zip(boundaries[:-1], boundaries[1:]):
             f = executor.submit(_pretokenize, input_path, begin, end, special_tokens)
             futs.append(f)
+        for f in as_completed(futs):
+            counts = f.result()
+            for k, v in counts.items():
+                pretoken_counts[k] += v
