5ce99030039d74b05157f7ea578b1f08e967ef29
Ye Kuang <yekuang.ky@gmail.com>
2026-06-28 17:10:37 +0800

    wip

--- Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/bpe.py
@@ -1,18 +1,27 @@
-
+import regex as re
 import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor
+from collections import defaultdict
 
 from .pretokenization_example import find_chunk_boundaries
 
-def _pretokenize(
-      input_path: str,
-      begin: int,
-      end: int,
-      special_tokens: list[str]
-):
-   special_tokens.sort(key=lambda t: -len(t))
-   
-   pass
+
+def _pretokenize(input_path: str, begin: int, end: int, special_tokens: list[str]):
+    special_tokens.sort(key=lambda t: -len(t))
+    re_special_tokens = "|".join([re.escape(t) for t in special_tokens])
+    with open(input_path) as f:
+        f.seek(begin)
+        chunk = f.read(end - begin)
+        chunk_splits = re.split(re_special_tokens, chunk)
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    pretokens = []
+    counts = defaultdict(int)
+    for s in chunk_splits:
+        pretokens = re.findall(PAT, s)
+        for tok in pretokens:
+            counts[tok] += 1
+    return counts
+
 
 def train_bpe_tokenizer(
     input_path: str,
@@ -24,12 +33,10 @@ def train_bpe_tokenizer(
     num_workers = mp.cpu_count()
     desired_num_chunks = num_workers * 2
     with open(input_path, "rb") as f:
-      boundaries = find_chunk_boundaries(f, desired_num_chunks, special_tokens[0])
-    
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
-       futs = []
-       for i in range(len(boundaries)-1):
-          f = executor.submit(_pretokenize, input_path, boundaries[i], boundaries[i+1], special_tokens)
-          futs.append(f)
+        boundaries = find_chunk_boundaries(f, desired_num_chunks, special_tokens[0])
 
-    
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        futs = []
+        for i in range(len(boundaries) - 1):
+            f = executor.submit(_pretokenize, input_path, boundaries[i], boundaries[i + 1], special_tokens)
+            futs.append(f)
