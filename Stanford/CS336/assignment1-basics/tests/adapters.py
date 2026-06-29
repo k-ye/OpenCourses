80b3dc2a232cd6eb175294cd9127b253e3900a41
yekuang <k-ye@users.noreply.github.com>
2026-06-29 14:49:26 +0800

    lab1: BPE tokenizer (#3)
    * wip
    
    * wip
    
    * accelerate token seq merge
    
    * from_cls stub

--- Stanford/CS336/assignment1-basics/tests/adapters.py
+++ Stanford/CS336/assignment1-basics/tests/adapters.py
@@ -9,7 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics.bpe import train_bpe_tokenizer
+from cs336_basics.bpe import train_bpe_tokenizer, Tokenizer
 
 def run_linear(
     d_in: int,
@@ -560,7 +560,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
