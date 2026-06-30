1ece908ca6efbae14c089f235c11ad7318255e7a
Ye Kuang <yekuang.ky@gmail.com>
2026-06-30 21:05:43 +0800

    rope

--- Stanford/CS336/assignment1-basics/cs336_basics/gpt.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/gpt.py
@@ -45,7 +45,6 @@ class RMSNorm(torch.nn.Module):
         g = torch.ones(d_model, device=device, dtype=dtype)
         self.g = torch.nn.Parameter(g)
 
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         in_dtype = x.dtype
         x = x.to(torch.float32)
@@ -57,10 +56,11 @@ class RMSNorm(torch.nn.Module):
         result = x * g / rms
         return result.to(in_dtype)
 
+
 class SwiGLU(torch.nn.Module):
-    def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None=None):
+    def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
         super().__init__()
-        
+
         self.w1 = Linear(d_model, d_ff, device, dtype)
         self.w2 = Linear(d_ff, d_model, device, dtype)
         self.w3 = Linear(d_model, d_ff, device, dtype)
@@ -70,4 +70,31 @@ class SwiGLU(torch.nn.Module):
         silu = a * torch.sigmoid(a)
         b = self.w3(x)
         return self.w2(silu * b)
-        
+
+
+class RotaryPositionalEmbedding(torch.nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device: torch.device | None = None):
+        super().__init__()
+
+        assert d_k % 2 == 0, f"d_k must be even, got {d_k}"
+        position_indices = torch.arange(0, max_seq_len, device=device)
+        position_indices = rearrange(position_indices, "d -> d 1")
+        pair_indices = torch.arange(0, (d_k // 2), device=device)
+        pair_indices = rearrange(pair_indices, "d -> 1 d")
+
+        angles = position_indices / theta ** ((2 * pair_indices) / d_k)
+        cos = torch.cos(angles)
+        sin = torch.sin(angles)
+        self.register_buffer("cos_table", cos, persistent=False)
+        self.register_buffer("sin_table", sin, persistent=False)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        cos = self.cos_table[token_positions]
+        sin = self.sin_table[token_positions]
+        x_even, x_odd = x[..., 0::2], x[..., 1::2]
+        rot_even = x_even * cos - x_odd * sin
+        rot_odd = x_even * sin + x_odd * cos
+        res = torch.empty_like(x)
+        res[..., 0::2] = rot_even
+        res[..., 1::2] = rot_odd
+        return res
--- Stanford/CS336/assignment1-basics/tests/adapters.py
+++ Stanford/CS336/assignment1-basics/tests/adapters.py
@@ -10,7 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.bpe import Tokenizer, train_bpe_tokenizer
-from cs336_basics.gpt import Linear, Embedding, RMSNorm, SwiGLU
+from cs336_basics.gpt import Linear, Embedding, RMSNorm, SwiGLU, RotaryPositionalEmbedding
 
 def run_linear(
     d_in: int,
@@ -210,7 +210,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = RotaryPositionalEmbedding(theta, d_k, max_seq_len, in_query_or_key.device)
+    return rope(in_query_or_key, token_positions)
 
 
 def run_transformer_block(
