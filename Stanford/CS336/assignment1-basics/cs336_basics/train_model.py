f0f866d9875dd8f8b0a521d89e4af171fdbcbf6d
yekuang <k-ye@users.noreply.github.com>
2026-07-06 16:56:07 +0800

    add muon optimizer (#13)
    * wip
    
    * muon wip
    
    * use muon

--- Stanford/CS336/assignment1-basics/cs336_basics/train_model.py
+++ Stanford/CS336/assignment1-basics/cs336_basics/train_model.py
@@ -11,6 +11,7 @@ import torch
 import random
 import numpy as np
 import json
+import regex as re
 
 
 def setup_logging(log_path: pathlib.Path):
@@ -71,6 +72,10 @@ def parse_args():
     parser.add_argument("--adamw-beta2", type=float, default=0.999)
     parser.add_argument("--adamw-eps", type=float, default=1e-8)
 
+    parser.add_argument("--use-muon", action="store_true", default=False)
+    parser.add_argument("--muon-lr", type=float, default=1e-2)
+    parser.add_argument("--muon-lr-min", type=float, default=1e-3)
+
     args = parser.parse_args()
     if args.lr_cosine_cycle_iters == -1:
         args.lr_cosine_cycle_iters = args.max_iters
@@ -83,6 +88,21 @@ def validate_args(args):
 
     if args.d_model % args.num_heads != 0:
         raise ValueError("Bad num_heads")
+
+
+def partition_opt_params_for_muon(model: torch.nn.Module) -> tuple[list[torch.nn.Parameter], list[torch.nn.Parameter]]:
+    muon_params: list[torch.nn.Parameter] = []
+    others_params: list[torch.nn.Parameter] = []
+    muon_pat = re.compile(r"layers\.\d+\.(attn|ffn)")
+    for name, p in model.named_parameters():
+        for_muon = muon_pat.match(name) is not None
+        # print(f"param: name={name} shape={p.shape} for_muon={for_muon}")
+        if for_muon:
+            assert p.ndim == 2
+            muon_params.append(p)
+        else:
+            others_params.append(p)
+    return muon_params, others_params
 
 
 def main():
@@ -104,6 +124,7 @@ def main():
     device = pick_device(args.device)
     dtype = pick_dtype(device, args.dtype)
 
+    torch.set_float32_matmul_precision("high")
     model = TransformerLM(
         vocab_size=args.vocab_size,
         context_length=args.context_length,
@@ -116,13 +137,29 @@ def main():
         dtype=dtype,
         model_dir=out_dir,
     )
-    optimizer = AdamW(
-        params=model.parameters(),
-        lr=args.lr,
-        weight_decay=args.adamw_weight_decay,
-        betas=(args.adamw_beta1, args.adamw_beta2),
-        eps=args.adamw_eps,
-    )
+
+    if args.use_muon:
+        muon_params, others_params = partition_opt_params_for_muon(model)
+        muon_opt = torch.optim.Muon(
+            muon_params,
+            lr=args.muon_lr,
+        )
+        adamw_opt = AdamW(
+            params=others_params,
+            lr=args.lr,
+            weight_decay=args.adamw_weight_decay,
+            betas=(args.adamw_beta1, args.adamw_beta2),
+            eps=args.adamw_eps,
+        )
+    else:
+        muon_opt = None
+        adamw_opt = AdamW(
+            params=model.parameters(),
+            lr=args.lr,
+            weight_decay=args.adamw_weight_decay,
+            betas=(args.adamw_beta1, args.adamw_beta2),
+            eps=args.adamw_eps,
+        )
 
     train_data = np.load(args.train_data, mmap_mode="r")
     val_data = np.load(args.val_data, mmap_mode="r")
@@ -130,6 +167,7 @@ def main():
     logging.info("Loaded training dataset, shape=%s dtype=%s", train_data.shape, train_data.dtype)
     logging.info("Loaded validation dataset, shape=%s dtype=%s", val_data.shape, val_data.dtype)
 
+    model = torch.compile(model)
     for it in range(args.max_iters):
         lr = calc_lr_cosine_schedule(
             it=it,
@@ -138,18 +176,32 @@ def main():
             warmup_iters=args.lr_warmup_iters,
             cosine_cycle_iters=args.lr_cosine_cycle_iters,
         )
-        for group in optimizer.param_groups:
+        muon_lr = calc_lr_cosine_schedule(
+            it=it,
+            max_learning_rate=args.muon_lr,
+            min_learning_rate=args.muon_lr_min,
+            warmup_iters=args.lr_warmup_iters,
+            cosine_cycle_iters=args.lr_cosine_cycle_iters,
+        )
+        for group in adamw_opt.param_groups:
             group["lr"] = lr
+        if muon_opt:
+            for group in muon_opt.param_groups:
+                group["lr"] = muon_lr
 
         x, y = get_batch(train_data, args.batch_size, args.context_length, str(device))
 
-        optimizer.zero_grad()
+        adamw_opt.zero_grad()
+        if muon_opt:
+            muon_opt.zero_grad()
         logits = model(x)
         loss = calc_cross_entropy(logits, y)
         loss.backward()
 
         do_gradient_clipping(model.parameters(), args.grad_clipping)
-        optimizer.step()
+        adamw_opt.step()
+        if muon_opt:
+            muon_opt.step()
 
         if it % args.log_interval == 0:
             logging.info("iter=%d train_loss=%.4f learning_rate=%g", it, loss.item(), lr)
@@ -169,11 +221,11 @@ def main():
 
         if it > 0 and it % args.checkpoint_interval == 0:
             ckpt_path = out_dir / f"ckpt_iter{it}.pt"
-            save_checkpoint(model, optimizer, it, ckpt_path)
+            save_checkpoint(model, adamw_opt, it, ckpt_path, muon_optimizer=muon_opt)
             logging.info("saved checkpoint: %s", ckpt_path)
 
     final_path = out_dir / "ckpt_final.pt"
-    save_checkpoint(model, optimizer, args.max_iters, final_path)
+    save_checkpoint(model, adamw_opt, args.max_iters, final_path, muon_optimizer=muon_opt)
     logging.info("saved final checkpoint: %s", final_path)
 
 
