28f8896780d5e9d004094c0a6d1c564be8b4ab05
Ye Kuang <yekuang.ky@gmail.com>
2026-07-10 10:19:38 +0800

    k v

--- Stanford/CS336/assignment2-systems/cs336_systems/flash_attention.py
+++ Stanford/CS336/assignment2-systems/cs336_systems/flash_attention.py
@@ -3,7 +3,8 @@ from torch.autograd.function import FunctionCtx
 
 
 class FlashAttentionFunc(torch.autograd.Function):
-    TILE_SIZE = 16
+    Q_TILE_SIZE = 16
+    K_TILE_SIZE = 16
 
     @staticmethod
     def forward(ctx: FunctionCtx, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, is_causal: bool):
@@ -11,33 +12,18 @@ class FlashAttentionFunc(torch.autograd.Function):
         # K, V: ... N d
         N_q, N_k = Q.shape[-2], K.shape[-2]
         head_dim = Q.shape[-1]
-        TILE_SIZE = FlashAttentionFunc.TILE_SIZE
-        assert N_q % TILE_SIZE == 0
-        assert N_k % TILE_SIZE == 0
-        T_q = N_q // TILE_SIZE
-        T_k = N_k // TILE_SIZE
+        B_q = FlashAttentionFunc.Q_TILE_SIZE
+        B_k = FlashAttentionFunc.K_TILE_SIZE
+        assert N_q % B_q == 0
+        assert N_k % B_k == 0
+        T_q = N_q // B_q
+        T_k = N_k // B_k
         for i in range(T_q):
-            Q_i = Q[..., i * TILE_SIZE : (i + 1) * TILE_SIZE, :]
+            Q_i = Q[..., i * B_q : (i + 1) * B_q, :]
             # *Q.shape[:-2] to make sure the batch dims are in
-            O_i = torch.zeros(
-                (*Q.shape[:-2], TILE_SIZE, head_dim),
-                dtype=torch.float32,
-                device=Q.device,
-            )
-            l_i = torch.zeros(
-                (
-                    *Q.shape[:-2],
-                    TILE_SIZE,
-                ),
-                dtype=torch.float32,
-                device=Q.device,
-            )
-            m_i = torch.full(
-                (
-                    *Q.shape[:-2],
-                    TILE_SIZE,
-                ),
-                -torch.inf,
-                dtype=torch.float32,
-                device=Q.device,
-            )
+            O_i = torch.zeros((*Q.shape[:-2], B_q, head_dim), dtype=torch.float32, device=Q.device)
+            l_i = torch.zeros((*Q.shape[:-2], B_q), dtype=torch.float32, device=Q.device)
+            m_i = torch.full((*Q.shape[:-2], B_q), -torch.inf, dtype=torch.float32, device=Q.device)
+            for j in range(T_k):
+                K_j = K[..., j * B_k : (j + 1) * B_k, :]
+                V_j = V[..., j * B_k : (j + 1) * B_k, :]
