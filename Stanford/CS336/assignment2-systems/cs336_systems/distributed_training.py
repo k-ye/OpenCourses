f52e9f75d72d81b217c48667a25a6726e6bfd57e
yekuang <k-ye@users.noreply.github.com>
2026-07-13 14:30:14 +0800

    lab2: DDP (#19)
    * wip
    
    * wip
    
    * async

---
+++ Stanford/CS336/assignment2-systems/cs336_systems/distributed_training.py
@@ -0,0 +1,53 @@
+import torch
+import torch.distributed as dist
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
+from typing import Literal
+
+
+class DDPContainer(torch.nn.Module):
+    def __init__(self, module: torch.nn.Module, policy: Literal["naive", "packed", "async"] = "async"):
+        super().__init__()
+        self.module = module
+        self.policy = policy
+        self.handles = []
+
+        def reduce_grad_async(p: torch.Tensor):
+            handle = dist.all_reduce(p.grad, op=dist.ReduceOp.AVG, async_op=True)
+            self.handles.append(handle)
+
+        for p in module.parameters():
+            dist.broadcast(p.data, src=0)
+            if p.requires_grad and policy == "async":
+                p.register_post_accumulate_grad_hook(reduce_grad_async)
+
+    def forward(self, *inputs, **kwargs):
+        return self.module(*inputs, **kwargs)
+
+    def finish_gradient_synchronization(self):
+        if self.policy == "naive":
+            self._finish_gradient_naive()
+        elif self.policy == "packed":
+            self._finish_gradient_packed()
+        else:
+            self._finish_gradient_async()
+
+    def _finish_gradient_naive(self):
+        for p in self.module.parameters():
+            if p.grad is not None:
+                dist.all_reduce(p.grad, op=dist.ReduceOp.AVG, async_op=False)
+
+    def _finish_gradient_packed(self):
+        grad_tensors = []
+        for p in self.module.parameters():
+            if p.grad is not None:
+                grad_tensors.append(p.grad)
+        flat_tensor = _flatten_dense_tensors(grad_tensors)
+        dist.all_reduce(flat_tensor, op=dist.ReduceOp.AVG, async_op=False)
+        restored_tensors = _unflatten_dense_tensors(flat_tensor, grad_tensors)
+        for dst, src in zip(grad_tensors, restored_tensors):
+            dst.copy_(src)
+
+    def _finish_gradient_async(self):
+        for h in self.handles:
+            h.wait()
+        self.handles.clear()
