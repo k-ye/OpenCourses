4c9b352a39a079f8dc9bd3e7e08fd880ae43d936
Ye Kuang <yekuang.ky@gmail.com>
2026-07-13 14:04:09 +0800

    wip

--- Stanford/CS336/assignment2-systems/cs336_systems/distributed_training.py
+++ Stanford/CS336/assignment2-systems/cs336_systems/distributed_training.py
@@ -1,5 +1,6 @@
 import torch
 import torch.distributed as dist
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 
 
 class DDPContainer(torch.nn.Module):
@@ -13,6 +14,20 @@ class DDPContainer(torch.nn.Module):
         return self.module(*inputs, **kwargs)
 
     def finish_gradient_synchronization(self):
+        self._finish_gradient_packed()
+
+    def _finish_gradient_naive(self):
         for p in self.module.parameters():
             if p.grad is not None:
                 dist.all_reduce(p.grad, op=dist.ReduceOp.AVG, async_op=False)
+
+    def _finish_gradient_packed(self):
+        grad_tensors = []
+        for p in self.module.parameters():
+            if p.grad is not None:
+                grad_tensors.append(p.grad)
+        flat_tensor = _flatten_dense_tensors(grad_tensors)
+        dist.all_reduce(flat_tensor, op=dist.ReduceOp.AVG, async_op=False)
+        restored_tensors = _unflatten_dense_tensors(flat_tensor, grad_tensors)
+        for dst, src in zip(grad_tensors, restored_tensors):
+            dst.copy_(src)
